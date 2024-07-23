4727265e5e233f833f79d01e89f74589e9719879
xerktyj <167881803+xerktyj@users.noreply.github.com>
2024-07-23 13:33:42 +0200

    add: la mia pizzeria old application

---
+++ spring-la-mia-pizzeria-web-api/src/main/java/it/pizzeria/SpringLaMiaPizzeriaWebApiApplication.java
@@ -0,0 +1,13 @@
+package it.pizzeria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringLaMiaPizzeriaWebApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringLaMiaPizzeriaWebApiApplication.class, args);
+	}
+
+}
